bf8f90e7f2cf7942b5e039b0dcd336113b08734c
yuandasp <sitiyuandap@gmail.com>
2023-04-03 20:01:54 +0700

    validator

--- server-side/database/index.js
+++ server-side/database/index.js
@@ -1,20 +1,20 @@
-const mysql = require('mysql2')
-const util = require('util')
+const mysql = require("mysql2");
+const util = require("util");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "sembilan9",
-    database: "db_ecommerce_exercise",
-    port: 3306
-})
+  host: "localhost",
+  user: "root",
+  password: "yuanda123",
+  database: "exercise_ecommerce",
+  port: 3306,
+});
 
 db.connect((err) => {
-    if (err) {
-        return console.error(`error: ${err.message}`)
-    }
-    console.log("Connected to mysql server")
-})
+  if (err) {
+    return console.error(`error: ${err.message}`);
+  }
+  console.log("Connected to mysql server");
+});
 
-const query = util.promisify(db.query).bind(db)
-module.exports = { db, query }
+const query = util.promisify(db.query).bind(db);
+module.exports = { db, query };
--- server-side/index.js
+++ server-side/index.js
@@ -1,14 +1,29 @@
-const express = require('express')
-const PORT = 8001
-const app = express()
-const {db} = require("./database")
-const cors = require('cors')
-const {authRoutes} = require('./routes')
+const express = require("express");
+const PORT = 8001;
+const app = express();
+const { db } = require("./database");
+const cors = require("cors");
+const { authRoutes } = require("./routes");
+const { body, validationResult } = require("express-validator");
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
-app.use(express.json())
+app.post(
+  "/validation",
+  body("email").isEmail(),
+  body("password").isLength({ min: 5 }),
+  async (req, res) => {
+    //   console.log("I'm validation");
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array() });
+    }
+    res.status(200).send(req.body);
+  }
+);
 
+app.use("/auth", authRoutes);
 // app.get('/user', async (req, res) => {
 //     let fetchQuery = 'SELECT * FROM users'
 //     db.query(fetchQuery, (err, result) => {
@@ -16,8 +31,6 @@ app.use(express.json())
 //     })
 // })
 
-app.use('/auth', authRoutes)
-
 app.listen(PORT, () => {
-    console.log("Server is running on port: " + PORT)
-})
+  console.log("Server is running on port: " + PORT);
+});
